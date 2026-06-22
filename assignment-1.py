05bf0e2450fc59bd87e1bc661be7ec1f78d49e2c
Anshika Tureha <anshika.tureha@novasarc.com>
2026-06-22 17:18:09 +0530

    Operators

---
+++ assignment-1.py
@@ -0,0 +1,74 @@
+
+"""
+1. Write a program to print “Hello Python” on the screen. 
+ans:-
+print("Hello Python")
+
+
+2. Take a name as input and print 
+Welcome <name> 
+ans:-
+name = input("Enter your name ")
+print ("Welcome",name)
+
+
+
+3. Take a number as input and print it. 
+ans:-
+num = int(input("Enter your number "))
+print(num)
+
+
+
+4. Take two numbers as input and print both numbers on separate lines.
+ans:-
+a = int(input(" Enter num A "))
+b = int(input(" Enter num B "))
+print(a)
+print(b)
+
+
+
+5. Take a name and age as input and print: 
+My name is ___ and my age is ___
+ans:-
+name = input(" Enter your name: ")
+age = input (" Enter your age: ")
+print("my name is",name,"and my age is",age)
+
+6. Take an integer as input and print its square.
+ans:-
+INT = int(input("Enter the integer "))
+print(INT**2)
+
+7. Take two integers as input and print their sum.
+ans:-
+a = int(input("Enter int A "))
+b = int(input("Enter int B "))
+print(a+b)
+
+8. Take two integers and print: 
+o Addition 
+o Subtraction 
+o Multiplication
+ans:-
+a = int(input("Enter int A "))
+b = int(input("Enter int B "))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+
+9. Take a number and print: 
+The number entered is <number>
+ans:
+num = 5
+print("The number entered is", num )
+
+10. Take an integer and print its double.
+ 
+"""
+a = int (input( "Enter the integer "))
+print(a*2)
+
+
+
