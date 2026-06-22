8ef51c82e85ae0120249d41dc4ac8d9474c930b3
Anshika Tureha <anshika.tureha@novasarc.com>
2026-06-22 17:20:11 +0530

    Guess Output

---
+++ assignment-2.py
@@ -0,0 +1,88 @@
+"""1. What is the output of the following code?
+a = 5
+b = 10
+print(a < b < 20)
+
+2. Predict the output:
+
+x = True
+y = False
+print(x + y * x) 
+
+3. What does this expression evaluate to?
+
+print(4 ** 0 ** 2) 
+
+4. Find the result of this bitwise operation:
+a = 12
+b = 5
+print(a ^ b) 
+
+5. Guess the output: 
+print((3 and 0) or (0 and 3)) 
+
+6. What's the output of this tricky comparison?
+
+print(256 is 256)
+print(257 is 257) 
+
+7. Evaluate this expression:  
+    a = 7
+    print(~a + 1)    
+ans:-
+~a= -(x+1)+1
+ = -(7+1)+1
+ = -8+1
+ = -7
+ 
+ 8. What will this print?
+a = True
+b = False
+print((a or b) and (not (a and b)))
+sol:
+true and (not(false)
+true and true
+output:true
+
+9. What's the output?
+
+print(10 > 5 == True)
+sol:
+10>5 and 5 == true        #but true == 1 hota hai kyuki true ki value 1 hoti hai 5 bhi true hai but eaqual to true nahi hai 
+true and false
+output:false
+
+10. Evaluate this expression:
+   print(2 + 3 * 4 ** 2 // 8 % 3)
+   
+Step 1: Operator Precedence
+
+Python follows this order:
+
+() Parentheses
+** Exponent
+*, /, //, % (Left to Right)
++, -
+
+11. What does this expression evaluate to?
+print(1 << 2 + 1)
+sol:
+1<<2+1
+1<<3
+1000
+output:
+8
+12. Predict the output:
+a = 3
+b = 2
+a *= b + 1
+=3*3
+= 9
+
+    """
+
+
+
+    
+
+
